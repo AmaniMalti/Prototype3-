a7897addc5c7938b5fc44d07b9f81b8cc1c6f4e1
AmaniMalti <amanimalti.95@gmail.com>
2021-03-21 15:36:12 -0400

    rigidbody

--- Assets/Script/movement.cs
+++ Assets/Script/movement.cs
@@ -24,6 +24,7 @@ public class movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
+            rb.AddRelativeForce(Vector3.up);
         }
     }
     void ProcessRotation()
