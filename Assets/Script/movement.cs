dd9ce87e1dcb469160fc47ff415937a391590762
AmaniMalti <amanimalti.95@gmail.com>
2021-03-20 23:43:39 -0400

    updating

--- Assets/Script/movement.cs
+++ Assets/Script/movement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        ProcessThrust();
+        ProcessRotation();
         
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+        }
+    }
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+        }
     }
 }
